0910ab2f49de208794873b322ca978ad44bf62f6
Jeppe Vammen Kristensen <jvk@vertica.dk>
2016-05-04 11:56:34 +0200

    Rough first draft

---
+++ Dirty/Display.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dirty
+{
+    /// <summary>
+    /// Interaction logic for Display.xaml
+    /// </summary>
+    public partial class Display : Window
+    {
+        public Display()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnDeactivated(EventArgs e)
+        {
+            base.OnDeactivated(e);
+            this.Topmost = true;
+
+        }
+    }
+}
---
+++ Dirty/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Dirty.Annotations;
+
+namespace Dirty
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private string _textToDisplay;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string TextToDisplay
+        {
+            get { return _textToDisplay; }
+            set
+            {
+               if (value == _textToDisplay) return;
+                _textToDisplay = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new ViewModel();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Display disp = new Display();
+            disp.DataContext = this.DataContext;
+            disp.Show();
+            disp.Topmost = true;
+        }
+    }
+}
